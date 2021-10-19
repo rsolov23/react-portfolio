dd1cae76bf50898869a1413bd97343992c358a00
Rachel Solov <rsolov23@yahoo.com>
2021-10-18 19:19:28 -0500

    deleted files for cleanup

--- src/App.js
+++ src/App.js
@@ -18,8 +18,9 @@ function App() {
           </video>
           <div>
             <Header />
+            <Route exact path="/" component={About} />
             <div>
-              <Route exact path="/" component={About} />
+              {/* <Route exact path="/" component={About} /> */}
               <Route exact path="/projects" component={Projects} />
               <Route exact path="/skills" component={Skills} />
               <Route exact path="/resume" component={Resume} />
